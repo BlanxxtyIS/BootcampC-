c0f30beb4fe3c845831c81526abad7b7ee7bf7f3
BlanxxtyIS <blanxxtyios@mail.ru>
2025-03-04 15:42:51 +0300

    Added and tested

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double[] temperature = new double[10]; //Фиксированный размер
+
+Random random = new Random();
+
+for (int i = 0; i < temperature.Length; i++) {
+    temperature[i] = random.NextDouble() * 40;
+    Console.WriteLine(temperature[i]);
+}
+
+
+List<double> names = []; //Обертка над массивом. Динамический массив.
+names.Add(9);
+names.Add(9);
+names.Add(9);
+names.Add(9);
+for (int i = 0; i < names.Count; i++) {
+    Console.WriteLine(names[i]);
+}
+Console.WriteLine(System.Math.Abs(10));
